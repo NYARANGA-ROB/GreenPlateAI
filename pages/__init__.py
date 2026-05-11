10d8547f2e909b0a9535bbdcc2e95e7a2b226a46
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-05-11 12:27:47 +0300

    Add files via upload

---
+++ pages/__init__.py
@@ -0,0 +1,15 @@
+"""
+Streamlit pages module for GreenPlateAI.
+
+This module contains additional Streamlit pages that can be
+loaded as separate pages in the multi-page application.
+"""
+
+# This file can be used to create additional Streamlit pages
+# For example, you could create separate pages like:
+
+# from .dashboard import dashboard_page
+# from .analytics import analytics_page
+# from .forecasting import forecasting_page
+
+__all__ = []
