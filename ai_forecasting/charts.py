70bbc3569c9b28581302dd6ace58e7116120175b
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-05-19 10:05:55 +0300

    Update charts.py

--- ai_forecasting/charts.py
+++ ai_forecasting/charts.py
@@ -9,7 +9,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 import pandas as pd
-
 import numpy as np
 
 from datetime import datetime, date, timedelta
