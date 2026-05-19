6c91c039157391162541d1804a1632f708c19503
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-05-19 09:04:29 +0300

    Ensure predictions are in list format
    Convert predictions to list if they are in numpy array format.

--- ai_forecasting/charts.py
+++ ai_forecasting/charts.py
@@ -42,7 +42,6 @@ class ForecastCharts:
             pred_values = forecast_data['predictions']
             
             if isinstance(pred_values, np.ndarray):
-                
                 pred_values = pred_values.tolist()
                 
             elif not isinstance(pred_values, list):
