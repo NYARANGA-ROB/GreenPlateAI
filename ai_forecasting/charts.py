87dcf00d8433b6968831f1bc696ed615fb9d2cbb
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-05-25 22:45:47 +0300

    Remove unnecessary blank line in charts.py

--- ai_forecasting/charts.py
+++ ai_forecasting/charts.py
@@ -11,7 +11,6 @@ from plotly.subplots import make_subplots
 import pandas as pd
 import numpy as np
 from datetime import datetime, date, timedelta
-
 from typing import Dict, List, Any, Optional, Tuple
 
 import logging
