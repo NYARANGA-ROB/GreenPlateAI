a3713e0f54f3d6be5c669e04df920893480ac3b4
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-05-26 08:02:15 +0300

    Update app_auth_example.py

--- app_auth_example.py
+++ app_auth_example.py
@@ -15,6 +15,7 @@ from auth import (
     auth_manager, check_authentication, get_current_user, is_authenticated,
 
     require_auth, require_admin, require_kitchen_staff,
+
     show_authenticated_sidebar, render_sidebar
 )
 from models.user_auth import UserRole
