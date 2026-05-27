ccb145f93f7cf885cb2c50f048fea5c976f8248d
Robert Nyaranga <101572972+NYARANGA-ROB@users.noreply.github.com>
2026-05-27 09:51:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ from database.connection import init_db, check_database_health
 from utils.helpers import format_currency, format_weight, format_percentage
 from auth.authenticator import authenticate_user, logout_user, get_current_user
 from dashboards.charts import create_waste_trend_chart, create_category_pie_chart
+
 from forecasting.models import get_demand_forecast
 
 from recommendations.engine import get_waste_reduction_recommendations
